437e394ad9334fd58d51955d82a8715f712279da
Adam7290 <rockingbeargaming@gmail.com>
2025-07-01 23:40:36 -0700

    gpu_verts_t

--- src/adm_framework/gpu.c
+++ src/adm_framework/gpu.c
@@ -1,9 +1,12 @@
+#define VEC_IMPLEMENTATION
 #include "gpu.h"
+#include "app.h"
 
 #include <adm_utils/panic.h>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stdarg.h>
 
 void _gpu_init(app_t* app) {
     PANIC_ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 1, "Failed to load GLAD.");
@@ -17,4 +20,74 @@ void gpu_clear(app_t* app, color_t color) {
         color.alpha / 255.0f
     );
     glClear(GL_COLOR_BUFFER_BIT);
+}
+
+const gpu_vert_decl_t gpu_vert_decl_new(app_t* app, usize count, ...) {
+    va_list args;
+    va_start(args, count);
+
+    gpu_vert_decl_t decl = _gpu_vert_decl_new(app->_arena);
+    _gpu_vert_decl_reserve(&decl, count);
+    for (int i = 0; i < count; i++) {
+        gpu_vert_attr_t attr = va_arg(args, gpu_vert_attr_t);
+        _gpu_vert_decl_push(&decl, attr);
+    }
+
+    va_end(args);
+
+    return decl;
+}
+
+PRIVATE void _gpu_verts_destroy_internal(gpu_verts_t* verts) {
+    glDeleteVertexArrays(1, &verts->_vao);
+    glDeleteBuffers(1, &verts->_vbo);
+}
+
+gpu_verts_t* gpu_verts_create(app_t* app, arena_t* arena, gpu_vert_decl_t* vert_decl) {
+    gpu_verts_t* verts = arena_defer(arena, _gpu_verts_destroy_internal, gpu_verts_t);
+    verts->_arena = arena;
+    verts->_app = app;
+
+    glGenVertexArrays(1, &verts->_vao);
+    glGenBuffers(1, &verts->_vbo);
+
+    glBindVertexArray(verts->_vao);
+
+
+    // Calculate stride
+    usize stride = 0;
+    for (int i = 0; i < _gpu_vert_decl_length(vert_decl); i++) {
+        gpu_vert_attr_t* vert = _gpu_vert_decl_get_unchecked(vert_decl, i);
+        stride += vert->size * sizeof(float); // MAGIC TYPE SIZEOF
+    }
+    verts->_vertex_size = stride;
+
+    // Actually setup opengl vertex attribs
+    usize offset = 0;
+    for (int i = 0; i < _gpu_vert_decl_length(vert_decl); i++) {
+        gpu_vert_attr_t* vert = _gpu_vert_decl_get_unchecked(vert_decl, i);
+        glVertexAttribPointer(i, vert->size, GL_FLOAT, GL_FALSE, stride, (void*)offset); // MAGIC GL_FLOAT
+        glEnableVertexAttribArray(i);
+        offset += vert->size * sizeof(float); // MAGIC TYPE SIZEOF
+    }
+
+    glBindVertexArray(0);
+
+    return verts;
+}
+
+void gpu_verts_upload(gpu_verts_t* verts, void* ptr, usize length) {
+    glBindBuffer(GL_ARRAY_BUFFER, verts->_vbo);
+    glBufferData(GL_ARRAY_BUFFER, length, ptr, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void gpu_verts_draw(gpu_verts_t* verts) {
+    glBindVertexArray(verts->_vao);
+    glDrawArrays(GL_TRIANGLES, 0, verts->_vertex_size);
+    glBindVertexArray(0);
+}
+
+void gpu_verts_destroy(gpu_verts_t* verts) {
+    arena_free(verts->_arena, verts);
 }
--- src/adm_framework/gpu.h
+++ src/adm_framework/gpu.h
@@ -3,9 +3,42 @@
 #include "color.h"
 
 typedef struct app_t app_t;
+typedef struct arena_t arena_t;
+
 typedef struct gpu_t {
 
 } gpu_t;
 
+typedef enum gpu_vert_attr_type_t {
+    GPU_VERT_ATTR_TYPE_FLOAT,
+    _gpu_vert_attr_type_count,
+} gpu_vert_attr_type_t;
+
+typedef struct gpu_vert_attr_t {
+    usize size;
+    gpu_vert_attr_type_t type;
+} gpu_vert_attr_t;
+
+#define VEC_TEMPLATE gpu_vert_attr_t
+#define VEC_TEMPLATE_PREFIX _gpu_vert_decl_
+#include <adm_utils/vec_impl.h>
+
+typedef struct _gpu_vert_decl_t gpu_vert_decl_t;
+typedef struct gpu_verts_t {
+    u32 _vbo, _vao;
+    usize _vertex_size;
+    app_t* _app;
+    arena_t* _arena;
+} gpu_verts_t;
+
+typedef struct gpu_shader_t {
+    u32 _handle;
+} gpu_shader_t;
+
 void _gpu_init(app_t* app);
 void gpu_clear(app_t* app, color_t color);
+const gpu_vert_decl_t gpu_vert_decl_new(app_t* app, usize count, ...);
+gpu_verts_t* gpu_verts_create(app_t* app, arena_t* arena, gpu_vert_decl_t* vert_decl);
+void gpu_verts_upload(gpu_verts_t* verts, void* ptr, usize length);
+void gpu_verts_draw(gpu_verts_t* verts);
+void gpu_verts_destroy(gpu_verts_t* verts);
